be6b49a2dc056485cd9867010b78782eddc526e3
a-sansara <dev@pluie.org>
2016-07-25 04:56:10 +0200

    adding image pluie/alpine-mysql

---
+++ pluie/alpine-mysql/install.d/10-mysql.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# pluie/docker-images - a-Sansara (https://github.com/a-sansara)
+
+apk --update add mysql mysql-client pwgen \
---
+++ pluie/alpine-mysql/pre-init.d/10-db.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# pluie/docker-images - a-Sansara (https://github.com/a-sansara)
+
+if [ ! -d "/run/mysqld" ]; then
+    mkdir -p /run/mysqld
+    chown -R mysql:mysql /run/mysqld
+fi
+
+if [ ! -d /var/lib/mysql/mysql ]; then
+    echo "[[ Initialize DB ]]"
+    chown -R mysql:mysql /var/lib/mysql
+    mysql_install_db --user=mysql > /dev/null
+
+    if [ ! -z "$MYSQL_ROOT_PASSWORD" ]; then
+        MYSQL_ROOT_PASSWORD=`pwgen -y -s 18 1`
+    fi
+
+    MYSQL_DATABASE=${MYSQL_DATABASE:-""}
+        MYSQL_USER=${MYSQL_USER:-""}
+    MYSQL_PASSWORD=${MYSQL_PASSWORD:-""}
+
+    tfile=`mktemp`
+    if [ ! -f "$tfile" ]; then
+        return 1
+    fi
+
+    cat << EOF > $tfile
+USE mysql;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+UPDATE user SET password=PASSWORD("$MYSQL_ROOT_PASSWORD") WHERE user='root';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
+UPDATE user SET password=PASSWORD("") WHERE user='root' AND host='localhost';
+FLUSH PRIVILEGES;
+EOF
+
+    if [ ! -z "$MYSQL_DATABASE" ]; then
+        echo "[[ Creating DB : $MYSQL_DATABASE ]]"
+        echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
+
+        if [ "$MYSQL_USER" != "" ]; then
+        echo "[[ Creating user : $MYSQL_USER ]]"
+        echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
+        fi
+    fi
+
+    /usr/bin/mysqld --user=mysql --bootstrap --verbose=0 < $tfile
+    rm -f $tfile
+fi
