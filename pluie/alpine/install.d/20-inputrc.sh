300de09c5a5075e4d702caaef94d2584eaf7e6e4
a-sansara <dev@pluie.org>
2016-07-29 15:22:12 +0200

    all : fix inputrc + mysql : add createdb script

---
+++ pluie/alpine/install.d/20-inputrc.sh
@@ -0,0 +1,13 @@
+# Keyboard shortcut : Ctrl+x followed by desired key 
+# refresh without bash logout : bind -f ~/.inputrc
+
+cat <<EOF > /etc/inputrc
+"\e[1;5C": forward-word             # ctrl + right
+"\e[1;5D": backward-word            # ctrl + left
+"\e[1~": beginning-of-line          # home
+"\e[3~": delete-char                # delete
+"\e[4~": end-of-line                # end
+"\e[5~": history-search-backward    # page-up
+"\e[6~": history-search-forward     # page-down
+EOF
+bind -f /etc/inputrc
